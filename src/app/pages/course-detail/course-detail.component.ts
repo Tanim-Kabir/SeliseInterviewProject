59046ff0bf4077bf29a1dfd9403bbd7365384087
Tanim Kabir <tanim.kabir@bengalmobileqa.com>
2025-05-17 17:13:46 +0600

    ongoing()

--- src/app/pages/course-detail/course-detail.component.ts
+++ src/app/pages/course-detail/course-detail.component.ts
@@ -18,7 +18,10 @@ export class CourseDetailComponent implements OnInit {
   prerequisites: Course[] = [];
   userData: LocalStorageDataModel =
     this.localStorageService.getLocalStorage('localStorageData');
-  enrolled: string[] = this.userData?.courses?.map((item) => item.id) || [];
+  enrolled: string[] =
+    this.localStorageService
+      .getLocalStorage('localStorageData')
+      .map((item: any) => item.id) || [];
 
   isPrerequisiteMatched: boolean = true;
 
